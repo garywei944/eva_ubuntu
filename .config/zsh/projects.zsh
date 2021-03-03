163fed1a13cf83001fd43f499d46290c0c82dfaa
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-03-02 21:03:04 -0500

    Remove alias peu

--- .config/zsh/projects.zsh
+++ .config/zsh/projects.zsh
@@ -11,7 +11,6 @@ __open_project() {
 	fi
 }
 
-alias peu='export GIT_DIR=~/.eva.git GIT_WORK_TREE=~ && cd ~'
 alias peva='__open_project ~/.eva'
 alias pe='__open_project ~/.spacemacs.d'
 alias pst='__open_project ~/.config/sublime-text-3/Packages'
