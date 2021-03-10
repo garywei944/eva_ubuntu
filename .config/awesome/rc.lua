0ffabc0b283834706f31fbc49be0cab534595030
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-03-09 21:55:31 -0500

    Change to default qtile config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -302,7 +302,7 @@ globalkeys = gears.table.join(awful.key({ modkey }, "s", hotkeys_popup.show_help
         { description = "open a terminal", group = "launcher" }),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
         { description = "reload awesome", group = "awesome" }),
-    awful.key({ modkey, "Shift" }, "q", awesome.quit,
+    awful.key({ "Control", "Mod1" }, "Delete", awesome.quit,
         { description = "quit awesome", group = "awesome" }),
 
     --    awful.key({ modkey }, "l", function() awful.tag.incmwfact(0.05) end,
