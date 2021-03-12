16649f94f58337feafb83d546dc4494e38b284cc
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-03-11 20:35:16 -0500

    Updating rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -190,8 +190,8 @@ awful.screen.connect_for_each_screen(function(s)
 	-- Each screen has its own tag table.
 	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
-	-- -- Create a promptbox for each screen
-	-- s.mypromptbox = awful.widget.prompt()
+	-- Create a promptbox for each screen
+	s.mypromptbox = awful.widget.prompt()
 	-- Create an imagebox widget which will contain an icon indicating which layout we're using.
 	-- We need one layoutbox per screen.
 	s.mylayoutbox = awful.widget.layoutbox(s)
@@ -224,7 +224,7 @@ awful.screen.connect_for_each_screen(function(s)
 			layout = wibox.layout.fixed.horizontal,
 			mylauncher,
 			s.mytaglist,
-			-- s.mypromptbox,
+			s.mypromptbox,
 		},
 		s.mytasklist, -- Middle widget
 		{ -- Right widgets
@@ -250,10 +250,13 @@ root.buttons(gears.table.join(
 globalkeys = gears.table.join(
 		awful.key({ modkey }, "s", hotkeys_popup.show_help,
 				{ description = "show help", group = "awesome" }),
-		-- awful.key({ modkey }, "Left", awful.tag.viewprev,
-		-- 		{ description = "view previous", group = "tag" }),
-		-- awful.key({ modkey }, "Right", awful.tag.viewnext,
-		-- 		{ description = "view next", group = "tag" }),
+		awful.key({ modkey }, "w", function() mymainmenu:show() end,
+				{ description = "show main menu", group = "awesome" }),
+
+		awful.key({ modkey }, "p", awful.tag.viewprev,
+				{ description = "view previous", group = "tag" }),
+		awful.key({ modkey }, "n", awful.tag.viewnext,
+				{ description = "view next", group = "tag" }),
 		-- awful.key({ modkey }, "Escape", awful.tag.history.restore,
 		-- 		{ description = "go back", group = "tag" }),
 
@@ -269,8 +272,6 @@ globalkeys = gears.table.join(
 				end,
 				{ description = "focus previous by index", group = "client" }
 		),
-		awful.key({ modkey }, "w", function() mymainmenu:show() end,
-				{ description = "show main menu", group = "awesome" }),
 
 		-- Layout manipulation
 		awful.key({ modkey, "Shift" }, "j", function() awful.client.swap.byidx(1) end,
@@ -333,30 +334,40 @@ globalkeys = gears.table.join(
 				end,
 				{ description = "restore minimized", group = "client" }),
 
-		-- -- Prompt
-		-- awful.key({ modkey }, "r", function() awful.screen.focused().mypromptbox:run() end,
-		-- 		{ description = "run prompt", group = "launcher" }),
+		-- Prompt
+		awful.key({ modkey }, "r", function() awful.screen.focused().mypromptbox:run() end,
+				{ description = "run prompt", group = "launcher" }),
 
-		-- awful.key({ modkey }, "x",
-		-- 		function()
-		-- 			awful.prompt.run {
-		-- 				prompt = "Run Lua code: ",
-		-- 				textbox = awful.screen.focused().mypromptbox.widget,
-		-- 				exe_callback = awful.util.eval,
-		-- 				history_path = awful.util.get_cache_dir() .. "/history_eval"
-		-- 			}
-		-- 		end,
-		-- 		{ description = "lua execute prompt", group = "awesome" }),
+		awful.key({ modkey }, "x",
+				function()
+					awful.prompt.run {
+						prompt = "Run Lua code: ",
+						textbox = awful.screen.focused().mypromptbox.widget,
+						exe_callback = awful.util.eval,
+						history_path = awful.util.get_cache_dir() .. "/history_eval"
+					}
+				end,
+				{ description = "lua execute prompt", group = "awesome" }),
 
-		-- Menubar
-		awful.key({ modkey }, "p", function() menubar.show() end,
-				{ description = "show the menubar", group = "launcher" }),
+		-- -- Menubar
+		-- awful.key({ modkey }, "p", function() menubar.show() end,
+		-- 		{ description = "show the menubar", group = "launcher" }),
 
 		-- Launcher
 		awful.key({ "Control" }, "space", function() awful.spawn("dmenu_run") end,
 				{ description = "run dmenu", group = "launcher" }),
 		awful.key({ "Control", "Shift" }, "Escape", function() awful.spawn("gnome-system-monitor") end,
-				{ description = "launch system monitor", group = "launcher" })
+				{ description = "launch system monitor", group = "launcher" }),
+		awful.key({ modkey }, "b", function() awful.spawn("google-chrome") end,
+				{ description = "launch google chrome", group = "launcher" }),
+		awful.key({ modkey }, "e", function() awful.spawn("nautilus") end,
+				{ description = "launch nautilus", group = "launcher" }),
+		awful.key({ modkey }, "KP_Left", function() awful.spawn("netease-cloud-music") end,
+				{ description = "launch netease cloud music", group = "launcher" }),
+		awful.key({ modkey }, "KP_Home", function() awful.spawn("/opt/apps/com.qq.weixin.deepin/files/run.sh -u") end,
+				{ description = "launch wechat", group = "launcher" }),
+		awful.key({ modkey }, "KP_Insert", function() awful.spawn("discord") end,
+				{ description = "launch discord", group = "launcher" })
 )
 
 clientkeys = gears.table.join(
@@ -372,37 +383,37 @@ clientkeys = gears.table.join(
 				{ description = "close", group = "client" }),
 		awful.key({ modkey, "Control" }, "space", awful.client.floating.toggle,
 				{ description = "toggle floating", group = "client" }),
-		-- awful.key({ modkey, "Control" }, "Return", function(c) c:swap(awful.client.getmaster()) end,
-		-- 		{ description = "move to master", group = "client" }),
+		awful.key({ modkey }, "Return", function(c) c:swap(awful.client.getmaster()) end,
+				{ description = "move to master", group = "client" }),
 		awful.key({ modkey }, "o", function(c) c:move_to_screen() end,
 				{ description = "move to screen", group = "client" }),
 		-- awful.key({ modkey }, "t", function(c) c.ontop = not c.ontop end,
 		-- 		{ description = "toggle keep on top", group = "client" }),
-		awful.key({ modkey }, "n",
-				function(c)
-					-- The client currently has the input focus, so it cannot be
-					-- minimized, since minimized clients can't have the focus.
-					c.minimized = true
-				end,
-				{ description = "minimize", group = "client" }),
-		awful.key({ modkey }, "m",
-				function(c)
-					c.maximized = not c.maximized
-					c:raise()
-				end,
-				{ description = "(un)maximize", group = "client" }),
-		awful.key({ modkey, "Control" }, "m",
-				function(c)
-					c.maximized_vertical = not c.maximized_vertical
-					c:raise()
-				end,
-				{ description = "(un)maximize vertically", group = "client" }),
-		awful.key({ modkey, "Shift" }, "m",
-				function(c)
-					c.maximized_horizontal = not c.maximized_horizontal
-					c:raise()
-				end,
-				{ description = "(un)maximize horizontally", group = "client" }),
+		-- awful.key({ modkey }, "n",
+		-- 		function(c)
+		-- 			-- The client currently has the input focus, so it cannot be
+		-- 			-- minimized, since minimized clients can't have the focus.
+		-- 			c.minimized = true
+		-- 		end,
+		-- 		{ description = "minimize", group = "client" }),
+		-- awful.key({ modkey }, "m",
+		-- 		function(c)
+		-- 			c.maximized = not c.maximized
+		-- 			c:raise()
+		-- 		end,
+		-- 		{ description = "(un)maximize", group = "client" }),
+		-- awful.key({ modkey, "Control" }, "m",
+		-- 		function(c)
+		-- 			c.maximized_vertical = not c.maximized_vertical
+		-- 			c:raise()
+		-- 		end,
+		-- 		{ description = "(un)maximize vertically", group = "client" }),
+		-- awful.key({ modkey, "Shift" }, "m",
+		-- 		function(c)
+		-- 			c.maximized_horizontal = not c.maximized_horizontal
+		-- 			c:raise()
+		-- 		end,
+		-- 		{ description = "(un)maximize horizontally", group = "client" }),
 		awful.key({ modkey }, "Down",
 				function(c)
 					if c.maximized then
