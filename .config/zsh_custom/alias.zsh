afc7be0ac9c0e2b268083ad2be9689faa506676a
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-04-24 20:48:24 -0400

    Add browser-sync alias

--- .config/zsh_custom/alias.zsh
+++ .config/zsh_custom/alias.zsh
@@ -7,8 +7,10 @@ alias rghi='rg --hidden --no-ignore'
 alias s='neofetch'
 
 # Commands
-alias bsst='browser-sync start --server --files "", \!.git'
-alias bsdy='browser-sync start --proxy "" --files "", \!.git'
+alias bs='browser-sync'
+alias bss='browser-sync start'
+alias bsexpress='browser-sync start -p "localhost:5000" -f "public"'
+
 alias gaignore='git ls-files -ci --exclude-standard -z | xargs -0 git rm --cached'
 alias reload='. ~/.zshrc'
 alias xaw='Xephyr -br -ac -noreset -screen 1920x1080 :1 & sleep 2 ; DISPLAY=:1 awesome'
